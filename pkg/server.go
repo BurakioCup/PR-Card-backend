c63f5b5a1c1aa8193be20f69f7ae6b0f5f518503
take-2405 <take12.wa@icloud.com>
2021-03-07 00:13:05 +0900

    [ADD]yaml

--- pkg/server.go
+++ pkg/server.go
@@ -2,6 +2,7 @@ package pkg
 
 import (
 	"PR-Card_backend/pkg/controller"
+	"PR-Card_backend/pkg/middleware"
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,15 +16,15 @@ func init() {
 	//アカウント作成
 	Server.POST("/create/auth", controller.CreateAuthHandler())
 	//ユーザが持っている名刺一覧
-	Server.GET("/read/cards", controller.ReadCardsHandler())
+	Server.GET("/read/cards", middleware.Authenticate(controller.ReadCardsHandler()))
 	//一覧から一つの名刺を詳細表示
-	Server.GET("/read/card", controller.ReadCardHandler())
+	Server.GET("/read/card", middleware.Authenticate(controller.ReadCardHandler()))
 	//自分の名刺を編集するために最初の状態を送信
-	Server.GET("/read/mycard", controller.ReadMycardHandler())
+	Server.GET("/read/mycard", middleware.Authenticate(controller.ReadMycardHandler()))
 	//自分の変更後の名刺を登録
-	Server.POST("/create/card", controller.CreateCard())
+	Server.POST("/create/card", middleware.Authenticate(controller.CreateCard()))
 	//自分の変更後の名刺を登録
-	Server.PUT("/update/card", controller.UpdateCard())
+	Server.PUT("/update/card", middleware.Authenticate(controller.UpdateCard()))
 	//名刺を表示するためのQRのリンクを送信
-	Server.GET("/read/qr", controller.ReadQR())
+	Server.GET("/read/qr", middleware.Authenticate(controller.ReadQR()))
 }
