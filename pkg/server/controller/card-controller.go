663ebeac21e409864b2f4d8c2c9806b48a4239f2
take-2405 <take12.wa@icloud.com>
2021-03-12 23:27:00 +0900

    [Fix]url params bind

--- pkg/server/controller/card-controller.go
+++ pkg/server/controller/card-controller.go
@@ -10,10 +10,7 @@ import (
 
 func ReadCardIDHandler()gin.HandlerFunc{
 	return func(c *gin.Context) {
-		//TODO 下記２行を決定
-		cardID := c.Param("cardID")
-		cardID=c.Query("cardID")
-		//fmt.Println(a)
+		cardID := c.Query("cardID")
 		if cardID==""{
 			log.Println("[ERROR] cardID is empty")
 			view.ReturnErrorResponse(
