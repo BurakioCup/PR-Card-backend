19552926f7ec0d2340837384c8da153d7c1ef10d
KeitaOsaki <keita.base.05@gmail.com>
2021-03-14 17:17:56 +0900

    [add]クエリ

--- pkg/server/model/dao/card-post.go
+++ pkg/server/model/dao/card-post.go
@@ -1,1 +1,26 @@
 package dao
+
+
+
+const (
+	InsertcardInfoQuery    = "INSERT INTO `cards` (id,name,image_path) VALUES (?,?,?)"
+	InsertcardWordInfoQuery = "INSERT INTO `card_my_word` (id,card_id,word) VALUES (?,?,?)"
+)
+
+
+type createWord struct {
+}
+
+func MakePostCardClientClient() createWord {
+	return createWord{}
+}
+
+func (info *createWord) Request(id,card_id,word string) error {
+
+	stmt, err := Conn.Prepare(InsertcardWordInfoQuery)
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(id,card_id,word)
+	return err
+}
