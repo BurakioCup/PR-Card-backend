f9d99f9a6cedb05df71e1613d37ee5965da618d6
KeitaOsaki <keita.base.05@gmail.com>
2021-03-16 01:28:03 +0900

    [add]データベースへの挿入処理

--- pkg/server/model/dao/card-post.go
+++ pkg/server/model/dao/card-post.go
@@ -1,7 +1,14 @@
 package dao
 
+import (
+	"database/sql"
+	"errors"
+	"log"
+)
+
 const (
 	InsertcardInfoQuery    = "INSERT INTO `cards` (id,face_image,status_image) VALUES (?,?,?)"
+	InsertCardDetailInfo   = "UPDATE `cards` SET `name_image`= ?,`tag_image`=?,`free_image`=? WHERE `id`=?;"
 )
 
 type createChart struct {
@@ -20,5 +27,32 @@ func (info *createChart) Request(id,face_image, status_image string) error {
 	}
 
 	_, err = stmt.Exec(id, face_image, status_image)
+	return err
+}
+
+type CreateDetail struct {
+}
+
+func MakeCreateDetailClient() CreateDetail {
+	return CreateDetail{}
+}
+
+func (info *CreateDetail) Request(userID,nameImage,tagImage,freeImage string) error {
+	var cardID string
+	var err error
+	row := Conn.QueryRow(SelectMyCardID, userID)
+	if err = row.Scan(&cardID); err != nil {
+		if err == sql.ErrNoRows {
+			return errors.New("Not created cards")
+		}
+		log.Println(err)
+		return err
+	}
+	stmt, err := Conn.Prepare(InsertCardDetailInfo)
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(nameImage,tagImage,freeImage,cardID)
 	return err
 }
