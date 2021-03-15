f07a7eee33d065d1f90ab9d1f8c1ce85fa11dfc5
KeitaOsaki <keita.base.05@gmail.com>
2021-03-15 14:02:34 +0900

    [fix]voew/card.goの構造体修正

--- pkg/server/view/card-post.go
+++ pkg/server/view/card-post.go
@@ -1,1 +1,12 @@
 package view
+
+
+type CreateCardResponse struct{
+	Id string `json:"id"`
+	UserName string `json:"userName"`
+	NameImage string `json:"nameImage"`
+	TagImage string `json:"tagImage"`
+	FaceImage string `json:"faceImage"`
+	StatusImage string `json:"statusImage"`
+	FreeImage string `json:"freeImage"`
+}
--- pkg/server/view/card.go
+++ pkg/server/view/card.go
@@ -21,7 +21,7 @@ type ReadCardResponse struct{
 	UserName string `json:"userName"`
 	FaceImage string `json:"faceImage"`
 	NickName string `json:"nickName"`
-	StatusImage string `json"statusImage"`
+	StatusImage string `json:"statusImage"`
 	Words [4]string `json:"words"`
 	FreeText string `json:"freeText"`
 }
